00bdce7dfce4317ef227dae21b978eacbb798f78
avilao <alexandre.vilao@gmail.com>
2017-12-06 07:52:39 +0000

    05 solved

---
+++ 05/2.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+import "os"
+import "bufio"
+import "strings"
+import "strconv"
+
+func main() {
+	// your code goes here
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var a []int
+	for scanner.Scan() {
+		s := scanner.Text()
+
+		for _, n := range strings.Fields(s) {
+			x, _ := strconv.Atoi(n)
+			a = append(a, x)
+		}
+
+	}
+
+	steps := 0
+
+	for i := 0; i >= 0 && i < len(a); {
+
+		steps++
+		add := a[i]
+		if add >= 3 {
+			a[i]--
+		} else {
+			a[i]++
+		}
+
+		i += add
+
+	}
+
+	fmt.Print(steps)
+
+}
