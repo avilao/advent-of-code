c99fc83191d5d948e1c8b668ef09091f6242337b
Alexandre Vilão <alexandre.vilao@gmail.com>
2018-12-09 00:27:19 +0000

    2018/07 - solved

--- 2018/07/2.py
+++ 2018/07/2.py
@@ -2,71 +2,63 @@ def processPathInfo(s):
   ns = s.split(' ')
   return [ns[1], ns[7], False]
 
-def findNextAvailableStart(paths):
-  for i, pathStart in enumerate(paths):
-    found = False
-    if pathStart[2]: #someone working on it
-      continue
-    for pathEnd in paths:
-      if pathStart[0] == pathEnd[1]:
-        found = True
-        break
-    if not found:
+def findNextAvailableStart():
+  for i, dep in enumerate(deps):
+    if dep != False and dep[0] == False and len(dep[1]) == 0:
       return i
   return -1
 
-def clearUselessPaths():
-  i = 0
-  while i < len(paths):
-    if marked.get(paths[i][0]) and marked.get(paths[i][1]):
-      del paths[i]
-    else:
-      i += 1
-  return
-
-def markPath(c):
-  if not marked.get(c):
-    marked[c] = True
-    return c
-  return ''
-
+def isFinished():
+  for d in deps:
+    if d != False:
+      return False
+  return True
   
-paths =  sorted(map(processPathInfo, open('input.txt').read().split("\n")))
+paths = map(processPathInfo, open('input.txt').read().split("\n"))
+deps = [False for x in range(26)]
 
-numWorkers = 2
-workerLoad = [[0, 0, -1] for x in range(numWorkers)] # [worked s, total sec, node index]
+for p in paths: # [boolWorkingInHere, list of deps]
+  pStart = ord(p[0]) - 65
+  pEnd = ord(p[1]) - 65
 
+  if deps[pStart] == False:
+    deps[pStart] = [False, []]
+  if deps[pEnd] == False:
+    deps[pEnd] = [False, []]
+  deps[pEnd][1].append(pStart)
+
+ 
+numWorkers = 5
+workerLoad = [[0, -1] for x in range(numWorkers)] # [worked s, total sec]
 final = ''
-marked = {}
 sec = 0
 
-while len(paths) > 1:
-  while sec < 9000 :
-    indexestoDelete = []
-    for w in workerLoad:
-      if w[1] == 0: # worker available
-        i = findNextAvailableStart(paths)
-        if i >= 0: # available path, start working
-          w[0] = 0
-          w[1] = ord(paths[i][0]) - 64 # target secs
-          w[2] = i
-          paths[i][2] = True
+while not isFinished():
+  for w in workerLoad:
+    if w[1] == -1: # worker available
+      i = findNextAvailableStart()
+      if i >= 0: # available path, start working
+        # print("sec %s, node %s" %(sec, i))
+        w[0] = 0
+        w[1] = i + 61
+        deps[i][0] = True
 
-    for w in workerLoad:
-      if w[1] >= 0: # it's working
-        w[0] += 1 # work on node
-        if w[0] == w[1]: # finished working? 
-          final += markPath(paths[w[2]])
-          w[1] = 0
-          w[2] = -1
+  for w in workerLoad:
+    if w[1] >= 0: # it's working
+      w[0] += 1 # work on node
+      if w[0] == w[1]: # finished working?
+        final  += chr(w[1] + 4)
 
+        # remove from all deps
+        cleanIndex = w[1] - 61
+        for d in deps:
+          if d and cleanIndex in d[1]:
+            d[1].remove(cleanIndex)
+        deps[cleanIndex] = False
+        w[1] = -1
 
-
-
-    clearUselessPaths()
-
-    
-
-
+  sec += 1
 
 print(final)
+print(sec)
+
