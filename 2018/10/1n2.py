0ac8d72c3109562b38166287d09c2d8881f7c852
Alexandre Vilao <alexandre.vilao@gmail.com>
2018-12-10 15:27:12 +0000

    2018/10 better code

--- 2018/10/1n2.py
+++ 2018/10/1n2.py
@@ -20,24 +20,21 @@ while not found:
     yMin = c[1] if c[1] < yMin else yMin
     yMax = c[1] if c[1] > yMax else yMax
 
-  if yMax - yMin > 10:
-    continue
+  if yMax - yMin <= 10:
+    found = True
 
-  found = True
-  y = yMin - 1
-  while y <= yMax + 1:
-    x = xMin - 1
-    while x <= xMax + 1:
-      ch = '.'
-      for c in coordinates:
-        if c[0] == x and c[1] == y:
-          ch = '#'
-          break
-      x += 1
-      sys.stdout.write(ch)
-    y += 1
-    sys.stdout.write('\n')
-  
+y = yMin - 1
+while y <= yMax + 1:
+  x = xMin - 1
+  while x <= xMax + 1:
+    ch = '.'
+    for c in coordinates:
+      if c[0] == x and c[1] == y:
+        ch = '#'
+        break
+    x += 1
+    sys.stdout.write(ch)
+  y += 1
   sys.stdout.write('\n')
-  print(n)
-  sys.stdout.flush()
+print(n)
+sys.stdout.flush()
