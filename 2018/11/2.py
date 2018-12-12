835f0aa75179dae131f55486a2b6145843ead589
Alexandre Vilao <alexandre.vilao@gmail.com>
2018-12-12 13:52:15 +0000

    2018/11 solved

--- 2018/11/2.py
+++ 2018/11/2.py
@@ -2,7 +2,6 @@ import math
 
 SERIAL = int(open('input.txt').read())
 GRID_SIZE = 300
-
 def energy(x, y):
   rackId = x + 10
   e = rackId * y
@@ -11,22 +10,28 @@ def energy(x, y):
   e = math.floor((e / 100) % 10)
   return int(e - 5)
 
-
 m = [[ energy(x + 1, y + 1) for y in range(GRID_SIZE)] for x in range(GRID_SIZE)]
 
-answer = [-5 * GRID_SIZE * GRID_SIZE, 0, 0, 0]
-
+sat = [[ 0 for y in range(GRID_SIZE)] for x in range(GRID_SIZE)]
 for y in range(GRID_SIZE):
-  print(y)
   for x in range(GRID_SIZE):
-    SQR_SIZE = 1
-    while (SQR_SIZE + x) <= GRID_SIZE and (SQR_SIZE + y) < GRID_SIZE:
-      energySum = 0
-      for b in range(SQR_SIZE):
-        for a in range(SQR_SIZE):
-          energySum += m[a+x][b+y]
-      if energySum > answer[0]:
-          answer = [energySum, x, y, SQR_SIZE]
-      SQR_SIZE += 1
-
-print(answer)
+      for b in range (y+1):
+        for a in range(x+1):
+          sat[x][y] += m[a][b]     
+      
+answer = [-5 * GRID_SIZE * GRID_SIZE, 0, 0, 0]
+for y in range(GRID_SIZE):
+  for x in range(GRID_SIZE):
+    for z in range(GRID_SIZE):
+      if z+y < GRID_SIZE and x+z < GRID_SIZE:
+        s = sat[x+z][y+z]
+        if x > 0 and y > 0:
+          s += sat[x-1][y-1]
+        if x > 0:
+          s -= sat[x-1][y+z]
+        if y > 0:
+          s -= sat[x+z][y-1]
+        if s > answer[0]:
+          answer = [s, x, y, z]
+        
+print("%s,%s,%s" % (answer[1], answer[2], answer[3])
