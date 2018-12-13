2652648c9020b3e67957c78db769dc2db8a199b0
Alexandre Vilao <alexandre.vilao@gmail.com>
2018-12-13 13:29:51 +0000

    2018/08 solved

--- 2018/08/1.py
+++ 2018/08/1.py
@@ -1,15 +1,16 @@
-arr = map(lambda x: int(x), open('input.txt').read().split(' '))
+def nodeValue(i):
+  global a, s
+  childCount = a[i]
+  metadataCount = a[i+1]
+  while childCount > 0:
+    nodeValue(i+2)
+    childCount -= 1
+  s += sum(a[i + 2:i+metadataCount + 2])
+  a = a[:i] + a[(i + metadataCount + 2):]
 
-print(arr)
-
-
-
-def nodeValue(a):
-  childCount = a[0]
-  metadataCount = a[1]
-  print(sum(a[-metadataCount:]))
-
-
-nodeValue(arr)
+a = map(lambda x: int(x), open('input.txt').read().split(' '))
+s = 0
+nodeValue(0)
+print(s)
 
 
