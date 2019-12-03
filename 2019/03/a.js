971c46837ee1b9c58be23d7e71685b28e988c792
Alexandre Vilao <alexandre.vilao@gmail.com>
2019-12-03 08:52:16 +0000

    2019 - Day 3 - solve part 1

--- 2019/03/a.js
+++ 2019/03/a.js
@@ -12,26 +12,29 @@ const d = {
   R: [1, 0],
 }
 const m = {}
+let iteration = 0;
 let minMd = undefined;
 
 rl.on("line", function(line) {
   wires = line.split(",").map(d => [d[0], parseInt(d.slice(1))])
   const p = [0, 0];  
- 
+  iteration += 1;
+
   wires.forEach(w => {
     for (let x = 0; x < w[1]; x++) {
       p[0] += d[w[0]][0];
       p[1] += d[w[0]][1];
-      m[p] = m[p] ? m[p] + 1 : 1; 
+      if(!m[p]) {
+        m[p] = {};
+      }
+      m[p][iteration] = m[p][iteration] ? m[p][iteration] + 1 : 1; 
     }
   });
 }).on("close", () => {
   Object.keys(m).forEach((k) => {
-    if (m[k] > 1) {
-      coordinates = k.split(',').map(v => parseInt(v));
-
-      md = Math.abs(coordinates[0]) + Math.abs(coordinates[1]) 
-
+    if (Object.keys(m[k]).length > 1) {
+      coordinates = k.split(',').map(v => Math.abs(parseInt(v)));
+      md = coordinates[0] + coordinates[1] 
       if (!minMd || md < minMd) {
         minMd = md;
       }
