e97660fe6eac34e9263cfc5bdc62d092f03a6029
Alexandre Vilao <alexandre.vilao@gmail.com>
2019-12-03 09:19:24 +0000

    2019 - Day 3 - solved - cleanup

--- 2019/03/ab.js
+++ 2019/03/ab.js
@@ -12,31 +12,29 @@ const d = {
   R: [1, 0],
 }
 const m = {}
-let iteration = 0;
-let minMd = undefined;
+let wireNumber = 0;
+minMd = undefined;
 let minSteps = undefined
 
 rl.on("line", function(line) {
-  wires = line.split(",").map(d => [d[0], parseInt(d.slice(1))])
+  wireTwists = line.split(",").map(d => [d[0], parseInt(d.slice(1))])
+  wireNumber += 1;
+  
   const p = [0, 0];  
-  iteration += 1;
   let steps = 1;
-
-  wires.forEach(w => {
+  wireTwists.forEach(w => {
     for (let x = 0; x < w[1]; x++, steps++) {
       p[0] += d[w[0]][0];
       p[1] += d[w[0]][1];
-      if(!m[p]) {
-        m[p] = {};
-      }
-      m[p][iteration] = m[p][iteration] ? m[p][iteration] : steps; 
+      if(!m[p]) m[p] = {};
+      if(!m[p][wireNumber]) m[p][wireNumber] = steps; 
     }
   });
 }).on("close", () => {
   Object.keys(m).forEach((k) => {
     if (Object.keys(m[k]).length > 1) {
-      coordinates = k.split(',').map(v => Math.abs(parseInt(v)));
-      md = coordinates[0] + coordinates[1] 
+      distance = k.split(',').map(v => Math.abs(parseInt(v)));
+      md = distance[0] + distance[1] 
       if (!minMd || md < minMd) {
         minMd = md;
       }
@@ -51,6 +49,6 @@ rl.on("line", function(line) {
       }
     }
   })
-  console.log(minMd);
-  console.log(minSteps);
+  console.log(minMd); // Part 1
+  console.log(minSteps); // Part 2
 });
