b1cf55aa75a449101955efc9e05c843ab4e8e411
Alexandre Vilao <alexandre.vilao@gmail.com>
2019-12-04 09:07:05 +0000

    2019 - Day 4 - Solved

---
+++ 2019/04/a.js
@@ -0,0 +1,26 @@
+var readline = require("readline");
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+function isValid(n) {
+  const a = (""+ n ).split("").map((t) => parseInt(t))
+  let adjacent = false;
+  for (let i = 0; i < a.length - 1; i++) {
+    if (a[i] > a[i+1]) return false;
+    if (a[i] === a[i+1]) adjacent = true;
+  }
+  return adjacent;
+}
+
+rl.on("line", function(line) {
+  range = line.split("-").map(v => parseInt(v));
+  let valid = 0;
+
+  for(let i = range[0]; i <= range[1]; i++) {
+    valid += isValid(i) ? 1 : 0;
+  }
+  console.log(valid);
+});
