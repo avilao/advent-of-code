af88f3654d162fea6e854cebd4fd51368f622432
Alexandre Vilao <alexandre.vilao@gmail.com>
2019-12-06 00:29:39 +0000

    2019 - Day 5 - Solved

--- 2019/05/b.js
+++ 2019/05/b.js
@@ -5,68 +5,84 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-const getNumberAt = (number, pos, l = 1) =>  Math.floor((number / Math.pow(10, pos - 1)) % Math.pow(10, l));
-const getValueFromOpcode = (p, mode) => mode === 0 ?  arr[arr[p]] : arr[p];
+const getNumberAt = (number, pos) =>  Math.floor((number / Math.pow(10, pos - 1)) % 10);
+const getValueFromOpcode = (n, mode) => (mode === 0 ? arr[arr[n]] : arr[n]);
 
 let arr = [];
-let input = 8;
+const input = 5;
 
 function doOp(p) {
   const instruction = arr[p];
-  const op = getNumberAt(instruction, 1, 2);
+  const op = getNumberAt(instruction, 1) + getNumberAt(instruction, 2) * 10;
   const o1mode  = getNumberAt(instruction, 3);
-  const o1 = getValueFromOpcode(p + 1, o1mode);
   const o2mode  = getNumberAt(instruction, 4);
-  const o2 = getValueFromOpcode(p + 2, o2mode);
   const o3mode  = getNumberAt(instruction, 5);
-  const o3 = getValueFromOpcode(p + 3, o3mode);
+  
 
-  let newPosition = p;
-
+  // console.log({p, instruction, op});
+  // console.log({o1, o1mode, o2, o2mode, o3});
+  
   switch (op) {
-    case 1:
+    case 1: {
+      const o1 = getValueFromOpcode(p + 1, o1mode);
+      const o2 = getValueFromOpcode(p + 2, o2mode);
+      const o3 = getValueFromOpcode(p + 3, 1);
       arr[o3] = o2 + o1;
-      newPosition += 4;
-      break;
-    case 2: 
+      return  p + 4;
+    }
+    case 2: {
+      const o1 = getValueFromOpcode(p + 1, o1mode);
+      const o2 = getValueFromOpcode(p + 2, o2mode);
+      const o3 = getValueFromOpcode(p + 3, 1);
       arr[o3] = o2 * o1;
-      newPosition += 4;
-      break;
-    case 3:
+      return  p + 4;
+    }
+    case 3: {
+      const o1 = getValueFromOpcode(p + 1, 1);
       arr[o1] = input;
-      newPosition += 2;
-      break;
-    case 4:
+      return  p + 2;
+    }
+    case 4: {
+      const o1 = getValueFromOpcode(p + 1, o1mode);
       console.log(o1);
-      newPosition += 2;
-      break;
-    case 5:
-      newPosition = o1 !== 0 ? o2 : newPosition + 3;
-      break;
-    case 6: 
-      newPosition = o1 === 0 ? o2 : newPosition + 3; 
-      break;
-    case 7:
+      return  p + 2;
+    }
+    case 5: {
+      const o1 = getValueFromOpcode(p + 1, o1mode);
+      const o2 = getValueFromOpcode(p + 2, o2mode);
+      return o1 !== 0 ? o2 : p + 3;
+    }
+    case 6: {
+      const o1 = getValueFromOpcode(p + 1, o1mode);
+      const o2 = getValueFromOpcode(p + 2, o2mode);
+      return o1 === 0 ? o2 : p + 3; 
+    }
+    case 7: {
+      const o1 = getValueFromOpcode(p + 1, o1mode);
+      const o2 = getValueFromOpcode(p + 2, o2mode);
+      const o3 = getValueFromOpcode(p + 3, 1);
       arr[o3] =  o1 < o2 ? 1 : 0;
-      newPosition += 4;
-      break;
+      return  p + 4;
+    }
     case 8:
-      arr[o3] =  o1 === o2 ? 1 : 0;
-      newPosition += 4;
-      break
+      const o1 = getValueFromOpcode(p + 1, o1mode);
+      const o2 = getValueFromOpcode(p + 2, o2mode);
+      const o3 = getValueFromOpcode(p + 3, 1);
+      arr[o3] = (o1 === o2 ? 1 : 0);
+      return  p + 4;
     case 99:
-      newPosition = 9999999999;
+      return 99999999999;
     default:
-      break;
+      return p + 4;
   }
-
-  return newPosition;
 }
 
 rl.on("line", function(line) {
   arr = line.split(",").map(v => parseInt(v));
 
+  // console.table(arr)
   for (let i = 0; i < arr.length;) {
     i = doOp(i);
+    // console.table(arr)
   }
-}).on("close", () => console.log(arr));
+});
