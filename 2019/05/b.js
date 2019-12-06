1301be124d614ba955f7b2417b78fce419af026e
Alexandre Vilao <alexandre.vilao@gmail.com>
2019-12-06 00:33:39 +0000

    2019 - Day 5 - cleanup

--- 2019/05/b.js
+++ 2019/05/b.js
@@ -16,58 +16,31 @@ function doOp(p) {
   const op = getNumberAt(instruction, 1) + getNumberAt(instruction, 2) * 10;
   const o1mode  = getNumberAt(instruction, 3);
   const o2mode  = getNumberAt(instruction, 4);
-  const o3mode  = getNumberAt(instruction, 5);
-  
-
-  // console.log({p, instruction, op});
-  // console.log({o1, o1mode, o2, o2mode, o3});
+  const o1 = getValueFromOpcode(p + 1, o1mode);
+  const o2 = getValueFromOpcode(p + 2, o2mode);
+  const o3 = getValueFromOpcode(p + 3, 1);
   
   switch (op) {
-    case 1: {
-      const o1 = getValueFromOpcode(p + 1, o1mode);
-      const o2 = getValueFromOpcode(p + 2, o2mode);
-      const o3 = getValueFromOpcode(p + 3, 1);
+    case 1:
       arr[o3] = o2 + o1;
       return  p + 4;
-    }
-    case 2: {
-      const o1 = getValueFromOpcode(p + 1, o1mode);
-      const o2 = getValueFromOpcode(p + 2, o2mode);
-      const o3 = getValueFromOpcode(p + 3, 1);
+    case 2: 
       arr[o3] = o2 * o1;
       return  p + 4;
-    }
-    case 3: {
-      const o1 = getValueFromOpcode(p + 1, 1);
-      arr[o1] = input;
+    case 3:
+      arr[getValueFromOpcode(p + 1, 1)] = input;
       return  p + 2;
-    }
-    case 4: {
-      const o1 = getValueFromOpcode(p + 1, o1mode);
+    case 4:
       console.log(o1);
       return  p + 2;
-    }
-    case 5: {
-      const o1 = getValueFromOpcode(p + 1, o1mode);
-      const o2 = getValueFromOpcode(p + 2, o2mode);
+    case 5:
       return o1 !== 0 ? o2 : p + 3;
-    }
-    case 6: {
-      const o1 = getValueFromOpcode(p + 1, o1mode);
-      const o2 = getValueFromOpcode(p + 2, o2mode);
+    case 6: 
       return o1 === 0 ? o2 : p + 3; 
-    }
-    case 7: {
-      const o1 = getValueFromOpcode(p + 1, o1mode);
-      const o2 = getValueFromOpcode(p + 2, o2mode);
-      const o3 = getValueFromOpcode(p + 3, 1);
+    case 7:
       arr[o3] =  o1 < o2 ? 1 : 0;
       return  p + 4;
-    }
     case 8:
-      const o1 = getValueFromOpcode(p + 1, o1mode);
-      const o2 = getValueFromOpcode(p + 2, o2mode);
-      const o3 = getValueFromOpcode(p + 3, 1);
       arr[o3] = (o1 === o2 ? 1 : 0);
       return  p + 4;
     case 99:
@@ -79,10 +52,7 @@ function doOp(p) {
 
 rl.on("line", function(line) {
   arr = line.split(",").map(v => parseInt(v));
-
-  // console.table(arr)
   for (let i = 0; i < arr.length;) {
     i = doOp(i);
-    // console.table(arr)
   }
 });
