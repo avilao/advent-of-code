284f62a2a10bfe41d21ba6ef79bdb77ac1c11609
Alexandre Vilao <alexandre.vilao@gmail.com>
2019-12-11 00:03:56 +0000

    2019 - Day 9 - wip

--- 2019/10/a.js
+++ 2019/10/a.js
@@ -1,20 +1,47 @@
 const utils = require('../utils');
 
 
-// function findLoS(a, b) {
-//   let visible = {}
+function findAsteroidsInLos(p) {
+  let count = [];
 
-//   for(let y = 0; y < m.length; y++) {
-//     for(let x = 0; x < m[y].length; x++) {
-    
-//       if(m[y][x] === '#') {
-//         visible[y-xa-x][]
-//       }
-//     }
-//   }
+  for(let by = 0; by < m.length; by++) {
+    for(let bx = 0; bx < m[by].length; bx++) {
+      if(m[by][bx] === '#' && !isSameCoordinate(p, [bx, by])) {
+        if(checkIfOnlyAsteroidsInLine(p, [bx, by])) {
+          count.push([bx, by]);
+        }
+      }
+    }
+  }
+  return count;
+}
+
+function checkIfOnlyAsteroidsInLine(p1, p2) {
+  const xRange = [p1[0], p2[0]].sort();
+  const yRange = [p1[1], p2[1]].sort();
+
+  for(let cy = yRange[0]; cy <= yRange[1]; cy++) {
+    for(let cx = xRange[0]; cx <= xRange[1]; cx++) {
+      if(m[cy][cx] === '#' && arePointsCollinear(p1, p2, [cx, cy]) && !isSameCoordinate(p1, [cx, cy]) && !isSameCoordinate(p2, [cx, cy])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 
-// }
+function arePointsCollinear(p1, p2, p3) {
+  return (p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1])) === 0.0;
+}
+
+function isSameCoordinate(p1, p2) {
+	if (p1.length !== p2.length) return false;
+	for (let i = 0; i < p1.length; i++) {
+		if (p1[i] !== p2[i]) return false;
+	}
+	return true;
+}
 
 let m = [];
 let result = {}
@@ -22,13 +49,30 @@ utils.rl.on("line", function(line) {
   m.push(line.split(""));
 }).on('close', function() {
   
-  for(let y = 0; y < m.length; y++) {
-    for(let x = 0; x < m[y].length; x++) {
-      result[`${x},${y}`] = m[y][x] === '.' ? 0 : findLoS(x, y); 
-
-      // console.log(x, y, utils.commonDenominators(x,y))
+  for(let ay = 0; ay < m.length; ay++) {
+    for(let ax = 0; ax < m[ay].length; ax++) {
+      result[`${ax},${ay}`] = m[ay][ax] === '.' ? 0 : findAsteroidsInLos([ax, ay]); 
     }
   }
 
-  console.log(result);
+  console.log(result)
+  for(let ay = 0; ay < m.length; ay++) {
+    let line = '';
+    for(let ax = 0; ax < m.length; ax++) {
+      line += result[`${ax},${ay}`].length || 0;
+    }
+    console.log(line);
+  }
+
+  let max = { value: 0, p: [] };
+  for(let y = 0; y < m.length; y++) {
+    for(let x = 0; x < m.length; x++) {
+      if (result[`${x},${y}`].length > max.value) {
+        max.value = result[`${x},${y}`].length;
+        max.p = [x, y];
+      } 
+    }
+  }
+
+  console.log(max);
 });
