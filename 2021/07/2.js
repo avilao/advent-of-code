7cd239abf4a59da87b6fd2bf5e1d60e5a998ca57
avilao <alexandre.vilao@gmail.com>
2021-12-08 11:05:50 +0000

    2021 - 07

---
+++ 2021/07/2.js
@@ -0,0 +1,30 @@
+const utils = require('../utils');
+
+let max = 0;
+let minTotalFuel = Infinity;
+
+async function main() {
+  const crabs = {};
+
+  (await utils.getLine())
+    .split(',')
+    .map((x) => parseInt(x))
+    .forEach((c) => {
+      if (c > max) max = c;
+      crabs[c] = crabs[c] ? crabs[c] + 1 : 1;
+    });
+
+  for (let x = 0; x <= max; x++) {
+    let totalFuel = 0;
+    for (let i = 0; i <= max; i++) {
+      const crabsInPosition = crabs[i] || 0;
+      const moves = Math.abs(x - i);
+      totalFuel += (crabsInPosition * (moves * (moves + 1))) / 2;
+    }
+
+    if (totalFuel < minTotalFuel) minTotalFuel = totalFuel;
+  }
+  console.log(minTotalFuel);
+}
+
+main();
