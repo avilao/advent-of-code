9c6a0ee571313a27f522ff2540cb8dee6a9d20f3
Alexandre Vilao <alexandre.vilao@sky.uk>
2024-12-05 19:30:24 +0000

    2024 day 5 part 2

--- 2024/05.js
+++ 2024/05.js
@@ -9,7 +9,6 @@ export function solve(input) {
 
   updateOrders.forEach((update) => {
     let isValid = true;
-
     for (let i = 0; i < update.length; i++) {
       const page = update[i];
       const rules = orderRules[page] || [];
@@ -28,13 +27,49 @@ export function solve(input) {
   return sum;
 }
 
+export function solveTwo(input) {
+  const [updateOrders, orderRules] = parseInput(input);
+
+  let sum = 0;
+
+  updateOrders.forEach((update) => {
+    let isValid = true;
+    for (let i = 0; i < update.length; i++) {
+      const page = update[i];
+      const rules = orderRules[page] || [];
+      const checkedElements = update.slice(0, i);
+
+      const brokenRules = getIntersections(rules, checkedElements);
+      if (brokenRules.length) {
+        isValid = false;
+        const indexToMove = findMinimumBrokenIndex(update, brokenRules);
+
+        update.splice(i, 1);
+        update.splice(indexToMove, 0, page);
+      }
+    }
+
+    if (!isValid) {
+      sum += parseInt(update[parseInt(update.length / 2)]);
+    }
+  });
+  return sum;
+}
+
+function findMinimumBrokenIndex(update, brokenRules) {
+  for (let i = 0; i < update.length; i++) {
+    if (brokenRules.includes(update[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 function parseInput(input) {
   const orderRules = {};
   const updateOrders = [];
 
   const lines = input.split('\n');
-
-  // console.log(lines);
 
   let i = 0;
   for (; i < lines.length; i++) {
--- 2024/05.test.js
+++ 2024/05.test.js
@@ -1,14 +1,14 @@
 import { readFile } from '../utils/filesystem';
-import { solve } from './05';
+import { solve, solveTwo } from './05';
 
 const input = readFile('05.input', import.meta.url);
 
 describe('2024 05', () => {
   it('Part One', () => {
-    expect(solve(input, 'XMAS')).toBe(143); // 143 4578
+    expect(solve(input)).toBe(4578); // 143
   });
 
-  // it('Part Two', () => {
-  //   expect(solve(input, 'MAS')).toBe(1796); // 9
-  // });
+  it('Part Two', () => {
+    expect(solveTwo(input)).toBe(6179); // 123
+  });
 });
